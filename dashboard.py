0d042384d20cf4723bdcbdac933918e1420980ce
Samir Sen <samirsen@stanford.edu>
2019-01-24 23:02:56 -0800

    adding demo material

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -10,23 +13,22 @@ import dash_html_components as html
 from dash.dependencies import Output, Event, Input
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+data_path = 'data/example_data.csv'
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# app_colors = {
-#     'background': '#0C0F0A',
-#     'text': '#FFFFFF',
-#     'sentiment-plot':'#41EAD4',
-#     'volume-bar':'#FBFC74',
-#     'someothercolor':'#FF206E',
-# }
+app_colors = {
+    'background': '#0C0F0A',
+    'text': '#FFFFFF',
+    'sentiment-plot':'#41EAD4',
+    'volume-bar':'#FBFC74',
+    'someothercolor':'#FF206E',
+}
 
 app.layout = html.Div(children=[
     html.H1(children='Regeneron AI'),
-
-    # Add styling
-    html.Div(children='''
-        Empowering building owners to be energy smart, more efficient, and provide cost savings.
+    html.H5(children='''
+        Empowering building owners to be energy smart, more efficient, and provide real cost savings.
     '''),
 
     html.Label('My Buildings', id='init-dropdown'),
@@ -36,7 +38,7 @@ app.layout = html.Div(children=[
             {'label': 'Branner Hall', 'value': 'Branner'},
             {'label': 'Lagunita Court', 'value': 'Lag'}
         ],
-        value=['Roble']
+        value=['Roble'],  style={'width':'65%','margin-left':10,'margin-right':10,'max-width':50000}
     ),
 
     # Dashboard div
@@ -55,20 +57,20 @@ app.layout = html.Div(children=[
             # generate_app_table(),
         ])
 
-    ], id='dashboard-main')
+    ], id='dashboard-main'),
 
-    # dcc.Graph(
-    #     id='example-graph',
-    #     figure={
-    #         'data': [
-    #             {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-    #             {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-    #         ],
-    #         'layout': {
-    #             'title': 'Dash Data Visualization'
-    #         }
-    #     }
-    # )
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
 ])
 
 def generate_summary_table():
